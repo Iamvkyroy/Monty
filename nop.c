6f5734b4fe8fe26fcc9ceb7b70d654846182fafe
LauraYarda <Laurayarda5@gmail.com>
2023-06-25 09:22:36 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,15 @@
+include "monty.h"
+/**
+  *f_nop- void or empty stack
+
+  *@head: head of stack
+
+  *@counter: line number
+
+  *Return: 0
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+(void)# counter;
+(void) head;
+}
